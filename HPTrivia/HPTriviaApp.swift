ddc87558f455bdcc90a251c55092c5461168d45d
Daniel Robles López <danrolodev@gmail.com>
2026-01-26 14:51:23 -0600

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by daniel.a.robles on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
